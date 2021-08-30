2a2846cf6bdd2ea08851dd50ee7bdda7bca960eb
Mohamed KEITA <medesko@gmail.com>
2021-08-30 14:23:44 +0200

    Route table Association with Public Subnet

--- main.tf
+++ main.tf
@@ -20,3 +20,39 @@ resource "aws_subnet" "privatesubnets" {
   vpc_id =  aws_vpc.Main.id
   cidr_block = "${var.private_subnets}"          
 }
+# Creating RT for Public Subnet
+# Traffic from Public Subnet reaches Internet via Internet Gateway
+resource "aws_route_table" "PublicRT" {    
+  vpc_id =  aws_vpc.Main.id
+  route {
+    cidr_block = "0.0.0.0/0"              
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+}
+# Creating RT for Private Subnet
+# Traffic from Private Subnet reaches Internet via NAT Gateway
+resource "aws_route_table" "PrivateRT" {   
+  vpc_id = aws_vpc.Main.id
+  route {
+    cidr_block = "0.0.0.0/0"            
+    nat_gateway_id = aws_nat_gateway.NATgw.id
+  }
+}
+#  Route table Association with Public Subnet's
+resource "aws_route_table_association" "PublicRTassociation" { 
+  subnet_id = aws_subnet.publicsubnets.id 
+  route_table_id = aws_route_table.PublicRT.id
+}
+#  Route table Association with Private Subnet's
+resource "aws_route_table_association" "PrivateRTassociation" {
+  subnet_id = aws_subnet.privatesubnets.id
+  route_table_id = aws_route_table.PrivateRT.id
+}
+resource "aws_eip" "nateIP" {
+  vpc   = true
+}
+#  Creating the NAT Gateway using subnet_id and allocation_id
+resource "aws_nat_gateway" "NATgw" {
+  allocation_id = aws_eip.nateIP.id
+  subnet_id = aws_subnet.publicsubnets.id
+}
