39092eb13734e9c794ed5c05200a7adc659ccd50
Mohamed KEITA <medesko@gmail.com>
2021-08-30 14:13:40 +0200

    add main vpc and aws_internet_gateway

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Creating VPC here  
+# Defining the CIDR block use 10.0.0.0/24 for demo
+resource "aws_vpc" "Main" {                
+  cidr_block       = var.main_vpc_cidr    
+  instance_tenancy = "default"
+}
+# Creating Internet Gateway
+# vpc_id will be generated after we create VPC
+resource "aws_internet_gateway" "IGW" {    
+  vpc_id =  aws_vpc.Main.id               
+}
