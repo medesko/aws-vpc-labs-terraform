24644f2605a72a4a33f8faf471cbf72d1af77d48
Mohamed KEITA <medesko@gmail.com>
2021-08-30 16:32:13 +0200

    set default region

--- vars.tf
+++ vars.tf
@@ -1,4 +1,8 @@
-variable "region" {}
+variable "region" {
+  type = string
+  description = "aws region to deploy to"
+  default = "eu-west-3"
+}
 variable "main_vpc_cidr" {}
 variable "public_subnets" {}
 variable "private_subnets" {}
